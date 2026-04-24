beeb654769fb057d0ee7a14978399de65190aa4c
Vicente_XK <vi2812009@gmail.com>
2026-04-24 15:37:48 -0300

    fotos dnv

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const mensagens = {
-  agora: `ESCREVE AQUI`,
-  motivos: `ESCREVE AQUI`,
-  rir: `ESCREVE AQUI`,
-  momentos: `ESCREVE AQUI`,
-  musica: `ESCREVE AQUI`,
-  segredo: `ESCREVE AQUI`,
-  noite: `ESCREVE AQUI`,
-  pensando: `ESCREVE AQUI`,
-  saudade: `ESCREVE AQUI`,
-  triste: `ESCREVE AQUI`,
-  brava: `ESCREVE AQUI`,
+  agora: `Estou escrevendo essa parte pela segunda vez kkkk, sao 12:14, talvez você se pergunte pq é a segunda vez e eu te digo, pq eu eu esqueci de salvar a primeira, aiai. mas enfim, eu nao façoa  menor ideia do que escrever aqui, o bruno me disse que o oq importa é a intenção mas eu sou ateu desse conceito, eu quero q seja especial, eu quero q seja complexo, eu quero, mas como vc mesmo diz: "nem tudo que a gente quer a gente pode ta querendo" mesmo assim eu vou fazer o máximo para que udo que eu escrever aqui, toque o mais fundo do seu meigo coração, te amo.`,
+  motivos: `O quanto eu gosto de você? aiai isso é até dificil de expressar em palvras, mas se eu fosse obrigado a responder essa pergunta eu diria: O que acontece com um pessoa que se ve sozinha, envolta nos seus pensamentos, encontra uma alma meiga que o faz se sentir amado? ele sente suas mãos tremerem, o coração bater acelerado, os olhos lagrimejar de alegria, alegria rara, a pupila dilatar, uma ansiedade, mas um ansiedade boa. essa pessoa era eu 122 dias atras, não é que eu não tivesse companhia ou me entia triste, mas com você em minha vida, sinto a pecinha que faltava foi encontrada. queria listar motivos aqui porém como eu disse, os 122 iam deixar os meus no chinelo kkkkk. brincadeiras a parte, nao se preocupe, eu vou falando esses motivos ao longo de meus dias com vc, nao tenho pressa.`,
+  rir: `A sla sua preta, como eu posso te fazer rir com texto? mó dificil kkkkkk, ta mas sei q vc diria q é possivel, então deixeme tentar. fiquei 10min pensando em muitas coisas, nenhuma me satisfez tanto, ent tome https://tinyurl.com/abrehehe`,
+  momentos: `E oq dizer de nossos momentos? nós ja vivemos tantas coisas juntos, cada mania, cada piada, conversa banal e bizarra, cada chamada, cada videozinho fofo ou engraçado mandado, todas essas coisas construiram momentos inesqueciveis em minha vida, momentos que nao esquecerei, afinal cada um deles mudou meu dia, deixou minha vida cada vez mais "noises"`,
+  musica: `Uma musica pra você? nem tem como eu escolher uma, entao decidi fazer uma playslist nada convencional, ela tem um propósito que nunca envolveu musica, consegue decifrar oq digo? https://open.spotify.com/playlist/0EsoTjcKVRkEGulkQ3VL14?si=371d54d84c6e4b2f&pt=c2da270fcb04252b35412ebeb5bb78cd`,
+  segredo: `Pensou q ia ter algo aqui? eu nunca escondi nada de tu e nunca esconderei, afinal meu segredo é seu segredo tbm shhhhh`,
+  noite: `Todo santo dia antes de você dormir, fechar os seus olhinhos e ligar o seu trator interioro, eu quero q saiba q tem um beta a 16h de caminhada de onde vc está que se importa muito, que te ama e está disposto a fazer tudo e mais um pouco por ti`,
+  pensando: `AWWWWWW q fofinho, se vc ta lendo isso é pq realmente pensou em mim, certo? não quero q leia isso sem pensar em mim.se vc continuou lendo é pq realmente ta pensando e mds, até parei de digitar na hora aqui, mas pensar que vc esta pensando em mim no seu pensamento e eu estou pensando em pensar sobre voce no pensamento me enche de alegria, uipiiii eu quero dar um bejo nessa tua nadega, alias lembrei de algo que não contei no segredinho, olha só, afinal tinha alo ent q mentiroso eu, mas enfim, eu acho q to virando o chubaka pq nasce muito pelo em mim, tu n viu minha bunda, slk`,
+  saudade: `ôh meu xamego, ta com saudade? ja mandou mensagem pra mim? se sim e eu n respondi, eu jaja respondo kkkk. cof cof, continuando, é normal sentir saudade eu sinto saudade todos os dias de vc, essa pessoinha tão especial, eu gostaria de dar um conselho mega zika sobre como inibir a saudade e coisa tal, mas n tem como, então se eu fosse dizer algo para fazer, escute nossa playlist, tenho certeza q vai ajudar a lembrar de mim, na verdade acho q isso daria mais saudade, vish, então sla, se for chorar manda audio akkkakkkakka, te amo`,
+  triste: `Minha pretinha ta chateada? poxa, hj não deve ter sido um dia fácil, nem tods os dias vão ser faceis mesmoe ta tudo bem, isso acontece, a gente não precisa estar feliz o tempo todo, mas me prometa uma coisa, não se esqueça q a sua "bengalinha" vulgo eu to sempre disposto a te amar, até em dias dificeis, se não mandou mensagem pra mim ainda, manda, as vezes desabafar vai te ajudar, não sou o melhor com conselhos, ou talvez vc pense que vai encher meu saco e eu digo NÂO, não vai, então por favor, me deixa ser o seu porto seguro, que prometo te escutar e te apoiar, até na maior tristeza.`,
+  brava: 'aiaiai acho q fiz porcaria dessa vez, devo ter feito alguma cagada pra vc estar aqui, então primeiramente eu gostaria de pedir as minhas mais sinceras desculpas pelo oq eu fiz, (palavras do proprio vicente do passado)` e segundo, eu queria dizer que provavelmente não foi minha intenção te deixar brava e irritada, as vezes eu nem percebi q fiz algo q te deixou assim, vc poderia me dar uma outra chance? prometo q posso resolver as coisas. e novamente eu digo, se não me disse oq aconteceu, me diga, para q eu possa entender. ó Deus, lhe de paciência, pq se der força, me mata`,
   abraco: `ESCREVE AQUI`,
-  especial: `ESCREVE AQUI`,
-  duvida: `ESCREVE AQUI`,
-  dia24: `ESCREVE AQUI`,
-  surpresa: `ESCREVE AQUI`
+  especial: `VOCÊ È ESPECIAL, E MUITO. sabe, todos os dias em penso em ti, nas tuas piadocas, bobeiras, manias, beleza e eu fico pensando em como vc é importante na minha vida, eu to tentando escrever algo que te faça sentir especial e a verdade é q não sou bom escrevendo, mas olhe só para esse site, somente ele demonstra o quao especial vc é para mim. eu demorei muito para fazer ele, foram impesilios atras de outros, mas eu persisti e tornei isso realidade, se fosse com qualquer outra coisa eu teria desistido, mas sabe pq eu n desisti? pq é pra vc e só esse fato ja me fez querer continuar, então não duvide, vc é especial!`,
+  duvida: `Se você está lendo isso, acredito que as coisas não estão na melhor situação, ou talvez vc só esteja ansiosa e pensativo, tbm me sinto assim as vezes, mas não importa o motivo que te fez entrar aqui, eu quero qu você entenda que nem sempre as coisas vão acontecer as mil maravilhas, sei que tu sabe disso mas não jogue a toelha. um tempo atras eu vi um grafico que mostrava duas vertentes, tempo e amor, conforme ia passando o tempo o amor subia e podia a chegar em estados catastróficos, e isso podia ser muito ruim, de vdd, mas conforme o tempo ia passando, os problemas vinham, o amor poderia diminuir e é ai q casais morrem, mas quem persiste, volta com o amor, mas não o amor catastrófico, mas um amor simples e verdadeiro, que vale a pena, e se tu esta lendo isso, saiba q vamos chegar no final desse grafico`,
+  dia24: `pq Você esta lendo isso antes do dia 24? posso saber?`,
+  surpresa: `Me caguei!!!`
 };
 
 function mostrar(id) {
